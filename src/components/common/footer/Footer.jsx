a5141f763011ae9a026ed8926a7bdf80fcd90ee8
Nehad <nehad.elsayed@robustastudio.com>
2025-09-28 00:03:10 +0300

    add portfolio name

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,4 +1,5 @@
-import logo from "../../../assets/logo.png";
+import { Link } from "react-scroll";
+import logo from "../../../assets/A-logo.ico";
 
 /* Footer nabLinks */
 const navItems = [
@@ -16,22 +17,29 @@ const Footer = () => {
   return (
     <div className="pt-25 md:pt-40 content max-2xl:px-3">
       <div className="flex max-md:flex-col justify-between mx-0 items-center h-full w-full text-neutral-200">
-        <a href="#" className="flex items-center border-0">
+        <Link
+          to="introduction"
+          smooth={true}
+          duration={900}
+          className="flex items-center border-0"
+        >
           <img src={logo} className="h-8 sm:h-14 rounded-2xl" alt="logo" />
           <p className="text-3xl sm:text-[32px] my-auto ms-[12px] font-semibold">
-            Brooklyn
+            Abdelrahman
           </p>
-        </a>
+        </Link>
         <div className="mx-7 max-md:my-7 text-center">
           {navItems.map((item) => (
-            <a
+            <Link
               key={item.id}
+              to={item.url.toLowerCase()}
+              smooth={true}
+              duration={900}
               className="mx-2 group inline-block relative w-fit text-[12px] sm:text-[16px]"
-              href={`#${item.url.toLowerCase()}`}
             >
               {item.name}
               <span className="absolute left-0 bottom-0 h-0.5 w-full bg-white scale-x-0 duration-300 group-hover:scale-x-100"></span>
-            </a>
+            </Link>
           ))}
         </div>
         <p className="text-[12px] sm:text-[16px]">
